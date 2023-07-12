83d78050051eb7b3b1ec7ea3e22c81f7cebaee2d
Bagongon , Trisha <bagongontrisha@gmail.com>
2023-07-12 21:50:16 +0800

    login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store, persistor } from './Components/Redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import Home from './Screens/Home';
+import Login from './Screens/Login';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-      </Routes>
-    </Router>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/home" element={<Home/>} />
+            <Route path="/login" element={<Login/>} />
+          </Routes>
+        </Router>
+    </PersistGate>
+    </Provider>
   );
 }
 
---
+++ src/api.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://onlinebugaw.pythonanywhere.com/"
+});
+
+export function createUserProfile(data, headers) {
+  return api.post("api/v1/accounts/users/", data, headers);
+}
+
+export function UserLogin(data, headers) {
+  return api.post("api/v1/accounts/token/login", data, headers);
+}

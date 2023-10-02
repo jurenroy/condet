6c931fed4626a6f4324f3bccba856d9660a8a6c7
jurenroy <jurenroyabragn91019@gmail.com>
2023-10-02 20:57:26 +0800

    gamayan gamay

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -24,7 +24,7 @@ function Header(props) {
     <div style={{ backgroundColor: '#060e57', padding: '1px', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', width: '100%', position: 'fixed', top: '0', left: '0', zIndex: '100' }}>
       <div style={{ display: 'flex', alignItems: 'center' }}>
         <img src={USTP} alt="USTP logo" style={{ width: '45px', height: '45px', margin: '5px', cursor: 'pointer' }} onClick={handleNavigateToHome} />
-        <h1 style={{ color: 'white', fontSize: '20px', marginLeft: '10px', cursor: 'pointer' }} onClick={handleNavigateToHome}>
+        <h1 style={{ color: 'white', fontSize: '15px', marginLeft: '10px', cursor: 'pointer' }} onClick={handleNavigateToHome}>
           UNIVERSITY OF SCIENCE AND TECHNOLOGY OF SOUTHERN PHILIPPINES
         </h1>
       </div>

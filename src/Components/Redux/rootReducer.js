d3025a739aae107f3ad65c21f6abde12f1179657
jurenroy <jurenroyabragn91019@gmail.com>
2024-02-29 11:42:08 +0800

    delete redux

--- src/Components/Redux/rootReducer.js
+++ src/Components/Redux/rootReducer.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
 import AuthReducer from './Auth/AuthSlice';
-import BulkReducer from './Bulk/BulkSlice';
 
 const rootReducer = combineReducers({
   auth: AuthReducer,
-  bulk: BulkReducer,
 });
 
 export default rootReducer;
